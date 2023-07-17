0893d1ca8e6cb2a9f06ce3369c01be3746d3547d
Tseng Hsi-Huang <9501sam@gmail.com>
2023-07-17 23:04:48 +0800

    init commit

---
+++ src/helper.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "helper.h"
+
+void help(void)
+{
+    printf("help\n");
+}
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "helper.h"
+
+int
+main(int argc, char **argv)
+{
+    printf("Hello world!\n");
+    help();
+
+    return 0;
+}
